bc615804fa13349d2663a500cba2d4f8efc0086f
shreyansh <code.shreyansh@gmail.com>
2026-05-19 08:58:19 +0530

    css fix

--- src/scenes/controlls.jsx
+++ src/scenes/controlls.jsx
@@ -10,43 +10,53 @@ const Controlls = () => {
   //   first();
   // }, []);
   return (
-    <div className="controlls-contianer">
-      <div className="steering-container">
-        <img src="/wheel.png" alt="Controlls" width={100} height={100} />
-        <div>
-          <h1>{lives}</h1>
+    <div>
+      <div className="controlls-contianer">
+        <div className="steering-container">
+          <img src="/wheel.png" alt="Controlls" width={100} height={100} />
+        </div>
+        <div id="photo" className="scroll-info">
+          <li>
+            <a
+              rel="noopener"
+              href="https://github.com/lifeanubis"
+              target="_blank"
+            >
+              github{' '}
+            </a>
+          </li>
+          {/* <li>score</li> */}
+          <li>
+            <a
+              rel="noopener"
+              href="/Shreyansh_Sharma_Resume_2026.pdf"
+              download={true}
+            >
+              resume &darr;
+            </a>
+          </li>
+          <li>
+            <a
+              rel="noopener"
+              href="https://my-world-3d.netlify.app/landing"
+              target="_blank"
+            >
+              portfolio{' '}
+            </a>
+          </li>
+          {/* <li>asd </li> */}
         </div>
       </div>
-      <div id="photo" className="scroll-info">
-        <li>
-          <a
-            rel="noopener"
-            href="https://github.com/lifeanubis"
-            target="_blank"
-          >
-            github{' '}
-          </a>
-        </li>
-        {/* <li>score</li> */}
-        <li>
-          <a
-            rel="noopener"
-            href="/Shreyansh_Sharma_Resume_2026.pdf"
-            download={true}
-          >
-            resume &darr;
-          </a>
-        </li>
-        <li>
-          <a
-            rel="noopener"
-            href="https://my-world-3d.netlify.app/landing"
-            target="_blank"
-          >
-            portfolio{' '}
-          </a>
-        </li>
-        {/* <li>asd </li> */}
+      <div className="game-info">
+        <div>
+          <h2>{lives}</h2>
+        </div>
+        <div className={lives <= 0 ? 'game-over' : 'game-on'}>
+          <h2>game over</h2>
+        </div>
+        <button className={lives <= 0 ? 'restart-btn' : 'restart-btn-off'}>
+          <h2>restart</h2>
+        </button>
       </div>
     </div>
   );
